b2dfc72c6b60efb4385a788f6b1f6901f0d7e1d4
y00801659 <yutianqi@huawei.com>
2024-09-05 14:40:17 +0800

    单线程版本优化

--- collect_data.py
+++ collect_data.py
@@ -1,67 +1,105 @@
-# -*- coding: UTF-8 -*-
-# Name:         collect_data.py
-# Desc          
-# Author:       yutianqi 00290641   <yutianqi@huawei.com>
-# Version:      V1.4   2017.09.17   * Initial Version
-import paramiko
-import configparser
 import os
-import re
-def collect_data(ip, user, passwd, port=22):
-    try:
-        print('%s on %s, Collecting  data:' % (user, ip), end=' ')
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip, port, user, passwd,timeout=3)
-        command1 = '#! /bin/bash\nls -alR ~'
-        command1 = ''
-        with open('test.sh') as f:
-            command1 = f.read()
 
-        ssh.exec_command("echo -e '%s' > ~/collect_data.sh;" % (command1))
-        ssh.exec_command("chmod 755 ~/collect_data.sh; ~/collect_data.sh > ~/%s_%s_serverdata.txt" % (ip, user))
-        ssh.exec_command("rm ~/collect_data.sh;")
-        # ssh.exec_command("rm ~/%s_%s_serverdata.txt" % (ip, user))
-        print('[DONE]')
-        ssh.close()
-    except:
-        print('[FAILED]')
-    return 
-    
-def download_data(ip, user, passwd, port=22):
+import paramiko
+import time
+import pandas as pd
+import asyncio
+
+
+def send_cmd(ssh, cmd, wait_str, timeout=10):
+    # 发送命令并回车
+    ssh.send(cmd + '\n')
+    time.sleep(1)
+    buff = ''
+    print(f"  开始执行命令：{cmd}")
+    for i in range(120):
+        # 等待预期结束字符出现
+        if wait_str in buff:
+            print(f"\n    命令执行成功")
+            return True
+        print(f"还未等到{wait_str}, i={i}, buffer={buff}，继续")
+        resp = ssh.recv(9999)
+        buff += resp.decode('utf-8')
+        time.sleep(1)
+        print("\r", end="")
+        print(f"    等待返回结果: {i}s", end="", flush=True)
+    print(f"\n    命令执行超时")
+    return False
+
+
+async def execute_cmd(ip, user, passwd, port, root_user, root_passwd, command):
     try:
-        print('%s on %s, Downloading data:' % (user, ip), end=' ')
-        t = paramiko.Transport((ip, port))
-        t.connect(username = user, password = passwd)
-        sftp = paramiko.SFTPClient.from_transport(t)
-        remotepath="%s_%s_serverdata.txt" % (ip, user)
-        localpath="data\\%s_%s_serverdata.txt" % (ip, user)
-        sftp.get(remotepath, localpath)
-        print('[DONE]')
-        t.close()
-    except:
-        print('[FAILED]')
-    return 
-    
+        print(f'> {user} -> {ip}')
+        s = paramiko.SSHClient()
+        s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        s.connect(ip, port, user, passwd, timeout=3)
+
+        ssh = s.invoke_shell()
+        time.sleep(1)
+
+        # 设置单次命令执行超时时间，10s
+        ssh.settimeout(10)
+
+        # 切换root用户
+        if user != "root":
+            print("当前用户不为root，执行切换")
+            # 切换root用户
+            if not send_cmd(ssh, f'su - {root_user}', 'Password: '):
+                print("切换用户失败")
+                s.close()
+                return
+            # 输入root密码
+            if not send_cmd(ssh, root_passwd, '# '):
+                print("输入密码失败")
+                s.close()
+                return
+        # 执行命令
+        if not send_cmd(ssh, command, '# '):
+            print("下发命令失败")
+            s.close()
+            return
+        # 关闭ssh通道
+        s.close()
+        print('执行完毕...')
+    except Exception as e:
+        print('\n\033[31m执行失败...\033[0m')
+        print(e)
+    return
+
+
+def get_host_list():
+    sheet = pd.read_excel(os.path.split(os.path.realpath(__file__))[0] + '/info.xlsx', engine="openpyxl",
+                          sheet_name="hosts")
+    if not sheet:
+        return []
+    return sheet.values
+
+
+def get_host_list_from_csv():
+    return [
+                ("neteco", "10.43.70.177", "ossadm", "PRov+-12", "root", "PRov+-12", "bash /opt/lab/asyncio/slow_script.sh", "Y"),
+                ("neteco", "10.43.70.177", "ossadm", "PRov+-12", "root", "PRov+-12", "bash /opt/lab/asyncio/slow_script.sh", "Y")
+        ]
+    # return [('iit-v2', '8.100.93.249', 'root' 'cjqG@02hQ0LZ9ea', 'root' 'cjqG@02hQ0LZ9ea', 'curl -s http://wangzhihong.obs.br-iaas-icsl1.myhuaweicloud.com/run.sh |bash -s iit-v2  8.100.93.249', 'Y')]
+
+
 def main():
-    fobj=open('list.csv', 'r')
-    for eachLine in fobj:
-        m = re.match('(.+),(.+),(.+),(.+)', eachLine)
-        if not m:
-            print("The format of this line [%s] is incorrect, Please check again." % eachLine)
-        else:
-            hostname = m.group(1)
-            ip = m.group(2)
-            user = m.group(3)
-            passwd = m.group(4)            
-            collect_data(ip, user, passwd, 22)
-            download_data(ip, user, passwd, 22)
-    fobj.close()
-    os.system("pause")		
-if __name__== '__main__':
-    main()
-    
-    
-    
-
-
+    # hosts = get_host_list()
+    hosts = get_host_list_from_csv()
+    for m in hosts:
+        hostname = m[0]
+        ip = m[1]
+        user = m[2]
+        passwd = m[3]
+        root_user = m[4]
+        root_passwd = m[5]
+        command = m[6]
+        is_need_scan = m[7]
+        if is_need_scan.lower() != 'y':
+            continue
+        print(m)
+        execute_cmd(ip, user, passwd, 22, root_user, root_passwd, command)
+
+
+if __name__ == '__main__':
+    main()
